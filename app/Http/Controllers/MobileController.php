12f53bd14a9d56d5e83e303b20b043c9d25d3a35
admin <fandofast@gmail.com>
2020-10-18 13:30:58 +0700

    F

--- app/Http/Controllers/MobileController.php
+++ app/Http/Controllers/MobileController.php
@@ -134,7 +134,7 @@ class MobileController extends Controller
 
         $baseapiurl=asset('storage/');
 
-        $song['playlist'] = Playlistsong::select('songs.id',DB::raw('CONCAT("'.$baseapiurl.'/songmp3/",songs.file) as filemp3'),'songs.duration as duration','songs.title as songname',DB::raw('CONCAT("'.$baseapiurl.'/songcover/",songs.cover) as songcover'),'artists.name as artistname',DB::raw('CONCAT("'.$baseapiurl.'/artist/",artists.cover) as artistcover'),'genres.name as genrename',DB::raw('CONCAT("'.$baseapiurl.'/genre/",genres.cover) as genrecover'),'albums.name as albumname',DB::raw('CONCAT("'.$baseapiurl.'/album/",albums.cover) as albumcover'))
+        $song['playlist'] = Playlistsong::select('songs.id',DB::raw('CONCAT("'.$baseapiurl.'/songmp3/",songs.file) as filemp3'),'songs.duration as duration','songs.title as songname',DB::raw('CONCAT("'.$baseapiurl.'/songcover/",songs.cover) as songcover'),'artists.name as artistname',DB::raw('CONCAT("'.$baseapiurl.'/artist/",artists.cover) as artistcover'),'genres.name as genrename',DB::raw('CONCAT("'.$baseapiurl.'/genre/",genres.cover) as genrecover'),'albums.name as albumname',DB::raw('CONCAT("'.$baseapiurl.'/album/",albums.cover) as albumcover'),DB::raw('CONCAT("'.$baseapiurl.'/songlirik/",songs.lyric) as lyric'))
 
         ->join('songs','songs.id','playlistsongs.song_id')
         ->join('artists','artists.id','songs.artist_id')
